adc76af8fc159ff520a0410685c8860959094261
Andrew Dorokhin <andrewnorilsk@gmail.com>
2016-03-09 19:41:49 +0000

    refactor collect_weather

--- collect_weather.py
+++ collect_weather.py
@@ -11,14 +11,29 @@ w = observation.get_weather()
 # print(w)
 
 
+def get_weather_wind_direction():
+    return str(azimuth.degree(round(json.loads(json.dumps(w.get_wind()), 1)['deg'])))
+
+
+def get_weather_wind_value():
+    return str(round(json.loads(json.dumps(w.get_wind()))['speed']))
+
+
+def get_weather_temperature():
+    return str(round(json.loads(json.dumps(w.get_temperature('celsius')))['temp']))
+
+
+def get_weather_humidity():
+    return str(round(json.loads(json.dumps(w.get_humidity()))))
+
+
 def weather_wind():
-    return "Ветер: " + str(azimuth.degree(round(json.loads(json.dumps(w.get_wind()), 1)['deg'])) + " " +
-                           str(round(json.loads(json.dumps(w.get_wind()))['speed'])) + " м/с")
+    return "Ветер: " + get_weather_wind_direction() + " " + get_weather_wind_value() + " м/с"
 
 
 def weather_temp():
-    return "Температура: " + str(round(json.loads(json.dumps(w.get_temperature('celsius')))['temp']))
+    return "Температура: " + get_weather_temperature()
 
 
 def weather_humidity():
-    return "Влажность: " + str(round(json.loads(json.dumps(w.get_humidity())))) + " %"
+    return "Влажность: " + get_weather_humidity() + " %"
